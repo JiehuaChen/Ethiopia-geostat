a4a57982c1a326a51e69f5afabe7801fa4c55883
Jiehua Chen <jiehua.chen@themathpath.com>
2014-05-30 12:44:02 +0300

    revised RK local R script.

--- code/bart+rk_code/RK_withbart_optimizedCV_wholeprocess_Rscript_local.R
+++ code/bart+rk_code/RK_withbart_optimizedCV_wholeprocess_Rscript_local.R
@@ -1,13 +1,11 @@
 #!/usr/bin/Rscript --vanilla
 
-
 # clean R workspace
 rm(list=ls())
 
 cmd.args <- commandArgs(trailingOnly=TRUE)
 
 soil_property <-  cmd.args[1]
-
 suppressPackageStartupMessages(require(BayesTree, quietly=T, warn.conflicts=FALSE))
 suppressPackageStartupMessages(require(gstat, quietly=T, warn.conflicts=FALSE))
 
@@ -53,24 +51,24 @@ library(BayesTree)
 
 bart.est <- bart(X_lm, Y_lm, x.test=predict_grid_1k_values.narm, sigdf=sigdf.est, sigquant=sigquant.est, k=k.est, ntree=ntree.est, ndpost=500, nskip=1000, keepevery=10)
 
-
 bart.predict.mean <-  apply(bart.est$yhat.test,2,mean)
 bart.predict.sd <- apply(bart.est$yhat.test, 2, sd)
 bart.predict <- bart.est$yhat.test
 
 # add the residuals of BART into the dataframe
 trained_est <- bart.est$yhat.train
-predict_map <- matrix(NA, length(bart.predict.mean), dim(trained_est)[2])
+predict_map <- matrix(NA, length(bart.predict.mean), dim(trained_est)[1])
+
+cat("Step 4.2: Ordinary Kriging on the BART residuals \n")
 
 for(k in 1:dim(trained_est)[1]){
-	lab_field.laea@data["bart.resi"] <- Y_lm - trained_est[k,]
-	cat("Step 4.2: Ordinary Kriging on the BART residuals \n")
+    cat(k, "Simulation \n")
 	
+	lab_field.laea@data["bart.resi"] <- Y_lm - trained_est[k,]
 	## STEP TWO: variogram for residuals ____________________________________
 	#compute exponential variogram of model residuals
 	vg.P.resi <- variogram(bart.resi ~ 1, data = lab_field.laea, cutoff=10E3)
 	vgmf.P.resi.exp <- fit.variogram(vg.P.resi, model = vgm(psill =0.02, model = "Exp", range = 10E3, nugget = 0.01))
-	
 	
 	#Now interpolate model residuals by simple kriging to the nodes of the grid.
 	#Simple kriging means that we know the model mean (mu), which is in this case 0.
@@ -78,30 +76,15 @@ for(k in 1:dim(trained_est)[1]){
 	coordinates(predict_grid_1k_coords) =~x+y
 	predict_grid_1k_coords@proj4string@projargs <- "+proj=laea +lat_0=5 +lon_0=20 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"
 	krige.P.resi<- krige(bart.resi ~1, lab_field.laea, newdata=predict_grid_1k_coords, model = vgmf.P.resi.exp, nmax = 100, beta=0, na.action=na.omit, nsim=1)
-		
-	# copy the kriging results to the SpatialPixelsDataFrame map_prediction
-	krige.P.resi$pred.resi<-krige.P.resi$var1.pred
-	krige.P.resi$variance.resi<- krige.P.resi$var1.var
-	
+
 	# compute the final prediction by adding the regression model prediction and the interpoalted residual
-	predlogP <- bart.predict[,k] +krige.P.resi$pred.resi
-	
-	# change to original value
-	krige.P.resi$RKpred_log <- predlogP
-	krige.P.resi$RKpred<-exp(predlogP)
+	predlogP <- bart.predict[k,] +krige.P.resi@data$sim1
 	predict_map[,k] <- exp(predlogP)
-	# to understand the variance of prediction, we can see lower and upper limits of prediction as alternative of variance map (var1.var)
-	alpha<-0.05 # this is 95% confidence level.
-	loglower<-predlogP - qnorm(p=1-alpha/2,mean=0,sd=1)*sqrt(bart.predict.sd^2+ krige.P.resi$var1.var)
-	logupper<-predlogP + qnorm(p=1-alpha/2,mean=0,sd=1)*sqrt(bart.predict.sd^2+ krige.P.resi$var1.var)
-	
-	# compute the variance of the regression+kriging prediction error by adding the regression prediction error variance and the kriging variace of the residuals
-	krige.P.resi$RKlower<-exp(loglower)
-	krige.P.resi$RKupper<-exp(logupper)
-	krige.P.resi$se_log <- sqrt(bart.predict.sd^2+ krige.P.resi$var1.var)
 }
 
 cat("Step 5: Write the estimated maps in disk, and those maps will be in map_results folder \n")
+krige.P.resi$mean = rowMeans(predict_map)
+krige.P.resi$sd = apply(predict_map, 1, sd)
 
 map_folder <- "../map_results/"
 setwd(map_folder)
@@ -109,37 +92,17 @@ setwd(map_folder)
 predlogP.new <- krige.P.resi
 gridded(predlogP.new)<-TRUE
 
-writeGDAL (
-  dataset=predlogP.new["RKupper"],
-  fname=paste("bart+rk_",soil_property,"_upper_ppm_20130726.tif", sep=""),
-  drivername= "GTiff",
-  type="Float32")
-
 
 writeGDAL (
-  dataset=predlogP.new["RKlower"],
-  fname=paste("bart+rk_",soil_property,"_lower_ppm_20130726.tif", sep=""),
-  drivername= "GTiff",
-  type="Float32")
-
-
-writeGDAL (
-  dataset=predlogP.new["RKpred"],
+  dataset=predlogP.new["mean"],
   fname=paste("bart+rk_",soil_property,"_mean_ppm_20130726.tif", sep=""),
   drivername= "GTiff",
   type="Float32")
 
 
 writeGDAL (
-  dataset=predlogP.new["RKpred_log"],
-  fname=paste("bart+rk_",soil_property,"_mean_log_ppm_20130726.tif", sep=""),
-  drivername= "GTiff",
-  type="Float32")
-
-
-writeGDAL (
-  dataset=predlogP.new["se_log"],
-  fname=paste("bart+rk_",soil_property,"_se_log_ppm_20130726.tif", sep=""),
+  dataset=predlogP.new["sd"],
+  fname=paste("bart+rk_",soil_property,"_sd_ppm_20130726.tif", sep=""),
   drivername= "GTiff",
   type="Float32")
 
